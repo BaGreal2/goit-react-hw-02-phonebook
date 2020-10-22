aa3199cc68355dcf776fca4472446fef3eea15e1
BaGreal2 <artemonik17@gmail.com>
2020-10-22 15:42:49 +0300

    Updates

---
+++ precache-manifest.74374568480fc6c3e9e0b2ab3eda342b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4691c4542b1d9f5d99aaed0d60440d",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "dc29066f9f664c490401",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.428863de.chunk.css"
+  },
+  {
+    "revision": "b37aaf1de5cebd252ab1",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.8d27a985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.8d27a985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc29066f9f664c490401",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.73b20d2c.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
